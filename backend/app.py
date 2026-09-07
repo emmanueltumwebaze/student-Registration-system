974232a0366a9203ace006f489ff9aaa4a0af08d
emmanueltumwebaze <emmanueltumwebaze96@gmail.com>
2026-09-07 19:21:33 +0300

    attendance-system

--- backend/app.py
+++ backend/app.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
             db.session.commit()
             print("Master administrator account successfully seeded into live database! 🎉")
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5000)
 
         
     
